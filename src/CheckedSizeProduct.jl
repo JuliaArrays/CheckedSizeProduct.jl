cf98dc65013b5d180b94b0500dbff88e8b6b94dd
Neven Sajko <s@purelymail.com>
2025-01-14 22:07:14 +0100

    init

--- src/CheckedSizeProduct.jl
+++ src/CheckedSizeProduct.jl
@@ -1,5 +1,62 @@
 module CheckedSizeProduct
+    export checked_size_product
 
-# Write your package code here.
+    """
+        checked_size_product(::Tuple{Any, Vararg})
 
+    In short; a safe product, suitable for computing, e.g., the value of
+    `length(dense_array)` from the value of `size(dense_array)`. In case everything
+    is as expected, return the value of the product of the elements of the given
+    tuple. Otherwise, return a `NamedTuple` containing Boolean values with
+    information on the encountered problems.
+
+    In more detail; given a nonempty tuple of `Integer`-likes:
+    1. Promote the elements to a common concrete type, say `T`. The user must
+       ensure `T` supports `Base.Checked.mul_with_overflow`, `iszero`, `typemax`
+       and `==`.
+    2. Calculate the product. In case no element is negative, no element is
+       `typemax(T)` and the product is representable as `T`, return the product.
+       Otherwise, return a `NamedTuple` containing three Boolean properties:
+        * `any_is_negative`: at least one element is negative
+        * `any_is_typemax`: at least one element is the maximum value representable
+          in the given type, as given by `typemax`
+        * `is_not_representable`: the product is not representable as `T`
+
+    Throws when given an empty tuple to avoid having to choose a default return
+    type arbitrarily.
+    """
+    function checked_size_product end
+
+    const NonemptyNTuple = Tuple{T, Vararg{T, N}} where {T, N}
+
+    function checked_dims_impl(have_overflow::Bool, a, ::Tuple{})
+        (a, have_overflow)
+    end
+    function checked_dims_impl(have_overflow::Bool, a::T, t::NonemptyNTuple{T, N}) where {T, N}
+        b = first(t)
+        (m, o) = Base.Checked.mul_with_overflow(a, b)
+        p = have_overflow | o
+        r = Base.tail(t)::NTuple{N, T}
+        checked_dims_impl(p, m, r)::Tuple{T, Bool}
+    end
+
+    function checked_size_product_impl(t::NonemptyNTuple{T, N}) where {T, N}
+        any_is_zero     = any(iszero, t)::Bool
+        any_is_negative = any(<(false), t)::Bool
+        any_is_typemax  = any(==(typemax(T)), t)::Bool
+        a = first(t)
+        r = Base.tail(t)::NTuple{N, T}
+        (product, have_overflow) = checked_dims_impl(false, a, r)::Tuple{T, Bool}
+        is_not_representable = have_overflow & !any_is_zero
+        if !(any_is_negative | any_is_typemax | is_not_representable)
+            product
+        else
+            (; any_is_negative, any_is_typemax, is_not_representable)
+        end
+    end
+
+    function checked_size_product(t::NonemptyNTuple{Any})
+        p = promote(t...)
+        checked_size_product_impl(p)  # avoid infinite recursion in case `promote` is quirky
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,76 @@
 using CheckedSizeProduct
 using Test
-using Aqua
+using Aqua: Aqua
 
 @testset "CheckedSizeProduct.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(CheckedSizeProduct)
     end
-    # Write your tests here.
+    @testset "empty input" begin
+        @test_throws Exception checked_size_product(())
+    end
+    @testset "singleton input" begin
+        for T ∈ (Int8, Int16, Int32, Int64, Int128)
+            for x ∈ 0:100
+                y = T(x)
+                @test y === checked_size_product((y,))
+            end
+        end
+    end
+    @testset "promotion" begin
+        @test 10 === checked_size_product((Int8(2), 5))
+        @test 10 === checked_size_product((2, Int8(5)))
+        @test Int16(10) === checked_size_product((Int8(2), Int16(5)))
+        @test Int16(10) === checked_size_product((Int16(2), Int8(5)))
+    end
+    @testset "input includes negative" begin
+        for t ∈ (
+            (-1,), (-1, 1), (1, -1), (0, -1), (-1, 0), (-1, -1),
+            (-1, typemax(Int)), (typemax(Int), -1),
+        )
+            @test (checked_size_product(t)).any_is_negative
+        end
+    end
+    @testset "input includes `typemax(T)`" begin
+        m = typemax(Int)
+        for t ∈ (
+            (m,), (m, m), (1, m), (m, 1), (0, m), (m, 0), (-1, m), (m, -1),
+        )
+            @test (checked_size_product(t)).any_is_typemax
+        end
+    end
+    @testset "overflows" begin
+        m = typemax(Int) ÷ 13
+        for t ∈ (
+            (m, m), (15, m), (m, 15), (m, m, m), (1, m, m), (m, 1, m), (m, m, 1),
+        )
+            @test (checked_size_product(t)).is_not_representable
+        end
+    end
+    @testset "overflows, but OK because of multiplication with zero" begin
+        m = typemax(Int) ÷ 13
+        for t ∈ (
+            (m, m, 0), (m, 0, m), (0, m, m),
+        )
+            @test 0 === checked_size_product(t)
+        end
+    end
+    @testset "exhaustive over small `Int8` values" begin
+        ran = 0:4
+        for x ∈ ran
+            for y ∈ ran
+                for z ∈ ran
+                    ref = Int8(prod((x, y, z)))
+                    a = Int8(x)
+                    b = Int8(y)
+                    c = Int8(z)
+                    @test ref === checked_size_product((a, b, c))
+                    @test ref === checked_size_product((true, a, b, c))
+                    @test ref === checked_size_product((a, true, b, c))
+                    @test ref === checked_size_product((a, b, true, c))
+                    @test ref === checked_size_product((a, b, c, true))
+                end
+            end
+        end
+    end
 end
