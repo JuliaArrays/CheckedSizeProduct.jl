9eacd9ef0491a7b4c022ff3d9c098558bdf99bdc
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-01-16 19:06:05 +0100

    better effects (#6)
    * tweak effect assumptions
        * use `:terminates_locally` instead of `:terminates_globally`
            * apply it independently of element type
        * apply `:nothrow` for known good types
    * custom `any`

--- src/CheckedSizeProduct.jl
+++ src/CheckedSizeProduct.jl
@@ -30,7 +30,15 @@ module CheckedSizeProduct
 
     const NonemptyNTuple = Tuple{T, Vararg{T, N}} where {T, N}
 
-    function checked_dims_impl(t::NonemptyNTuple)
+    macro assume_terminates_locally(x)
+        if isdefined(Base, Symbol("@assume_effects"))
+            :(Base.@assume_effects :terminates_locally $x)
+        else
+            x
+        end
+    end
+
+    @assume_terminates_locally function checked_dims_impl(t::NonemptyNTuple)
         a = first(t)
         have_overflow = false
         for i ∈ eachindex(t)[2:end]
@@ -42,25 +50,51 @@ module CheckedSizeProduct
         (a, have_overflow)
     end
 
-    const Terminates = Union{
+    @assume_terminates_locally function any_impl(f, t::NonemptyNTuple)
+        a = f(first(t))::Bool  # assuming no `missing`
+        for i ∈ eachindex(t)[2:end]
+            b = f(t[i])::Bool
+            a |= b
+        end
+        a
+    end
+
+    # define singleton types for these functions to be able to dispatch on them
+    function is_negative(x)
+        x < 0
+    end
+    function is_typemax(x)
+        x == typemax(x)
+    end
+
+    const HasGoodEffects = Union{
         Int8, Int16, Int32, Int64, Int128,
     }
 
     function checked_dims(t::NonemptyNTuple)
         checked_dims_impl(t)
     end
+    function any_(f, t::NonemptyNTuple)
+        any_impl(f, t)
+    end
     @static if isdefined(Base, Symbol("@assume_effects"))
-        Base.@assume_effects :terminates_globally function checked_dims(
-            t::(NonemptyNTuple{T} where {T <: Terminates}),
+        Base.@assume_effects :nothrow function checked_dims(
+            t::(NonemptyNTuple{T} where {T <: HasGoodEffects}),
         )
             checked_dims_impl(t)
+        end
+        Base.@assume_effects :nothrow function any_(
+            f::Union{typeof(iszero), typeof(is_negative), typeof(is_typemax)},
+            t::(NonemptyNTuple{T} where {T <: HasGoodEffects}),
+        )
+            any_impl(f, t)
         end
     end
 
     function checked_size_product_impl(t::NonemptyNTuple{T, N}) where {T, N}
-        any_is_zero     = any(iszero, t)::Bool
-        any_is_negative = any(<(false), t)::Bool
-        any_is_typemax  = any(==(typemax(T)), t)::Bool
+        any_is_zero = any_(iszero, t)
+        any_is_negative = any_(is_negative, t)
+        any_is_typemax = any_(is_typemax, t)
         (product, have_overflow) = checked_dims(t)
         is_not_representable = have_overflow & !any_is_zero
         if !(any_is_negative | any_is_typemax | is_not_representable)
